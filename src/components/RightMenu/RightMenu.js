b8a4b2ae1581bbacde51489b81ec27d9036341bb
Jmarchandev <j.marchand.dev@gmail.com>
2021-01-17 11:59:09 +0100

    some responsive changes

--- src/components/RightMenu/RightMenu.js
+++ src/components/RightMenu/RightMenu.js
@@ -13,9 +13,6 @@ import Account from './Account';
 import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 
 const useStyles = makeStyles((theme) => ({
   root: {
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -17,17 +17,28 @@ import { Typography } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
     root: {
+        padding: 0,
         marginTop: theme.sizing.marginTop,
         marginRight: theme.sizing.marginRight,
         marginLeft: theme.sizing.marginLeft,
+        [theme.breakpoints.down("xs")]: {
+            marginRight: 10,
+            marginLeft: 10,
+        },
     },
     card: {
         width: "100%",
         padding: 10,
+        [theme.breakpoints.down("xs")]: {
+            textAlign: 'center',
+        },
     },
     items: {
         display: 'flex',
         flexWrap: 'wrap',
+        [theme.breakpoints.down("xs")]: {
+            justifyContent: 'space-between',
+        },
     },
     gridItem: {
         display: 'flex',
@@ -51,7 +62,7 @@ export const Home = () => {
                                     key={equipment._id}
                                     className={classes.gridItem}
                                     item
-                                    sm="12" md="4" lg="4"
+                                    sm="6" md="4" lg="4"
                                 >
                                     <LedCard equipment={equipment} />
                                 </Grid>
