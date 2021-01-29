f39ea43621f606057201a40e007db7bfb30b56d5
Jmarchandev <j.marchand.dev@gmail.com>
2021-01-29 20:17:10 +0100

    change address socket

--- src/utils/sockets/LedRequest.js
+++ src/utils/sockets/LedRequest.js
@@ -4,7 +4,7 @@ import WebSocket from 'socket.io-client';
 // API requests
 import { getEquipmentById, updateEquipment } from '../api/Equipments';
 
-const socket = new WebSocket(/*process.env.REACT_APP_DEVICE_IP || */ 'localhost:8000', { transports: ['websocket'] });
+const socket = new WebSocket(process.env.REACT_APP_DEVICE_IP, { transports: ['websocket'] });
 
 // Request
 const reqToggleLedKey = 'REQ_TOGGLE_LED';
